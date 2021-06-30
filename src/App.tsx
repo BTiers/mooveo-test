6ec805642c9aaa9ae1cac11ff25010a498eb9879
BTiers <baptiste.tiers@epitech.eu>
2021-06-30 07:53:21 +0200

    Tests without comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-// import Exercice from "./TS/ExerciceResult";
+import Exercice from "./TS/ExerciceResult";
 // import BouncingStation from "./BouncingBall/BounceStation";
 // import Nationalize from "./API/Nationalize";
 
 function App() {
-  // return <Exercice />;
+  return <Exercice />;
   // return <BouncingStation />;
   // return <Nationalize />;
 }
